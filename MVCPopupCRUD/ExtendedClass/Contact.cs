b1f2b7d17e9af7a56508e0b21f123f7bad360172
BANGKOKAIR\kritawit <kritawitbunket@gmail.com>
2016-10-28 17:19:30 +0700

    Add model and ajax function for fetch data.

---
+++ MVCPopupCRUD/ExtendedClass/Contact.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCPopupCRUD
+{
+    [MetadataType(typeof(ContactMetaData))]
+    public partial class Contact
+    {
+
+        public string CountryName { get; set; }
+        public string StateName { get; set; }
+
+    }
+
+    public class ContactMetaData
+    {
+        [Required(ErrorMessage = "Contact Name required", AllowEmptyStrings = false)]
+        [Display(Name = "Contact Person")]
+        public string ContactPerson { get; set; }
+
+        [Required(ErrorMessage = "Contact No required", AllowEmptyStrings = false)]
+        [Display(Name = "Contact No")]
+        public string ContactNo { get; set; }
+
+        [Required(ErrorMessage = "Country required", AllowEmptyStrings = false)]
+        [Display(Name = "Country")]
+        public int CountryID { get; set; }
+
+        [Required(ErrorMessage = "State required", AllowEmptyStrings = false)]
+        [Display(Name = "State")]
+        public int StateID { get; set; }
+
+    }
+}
---
+++ MVCPopupCRUD/Scripts/dotnetawesome.CRUDContacts.js
@@ -0,0 +1,29 @@
+﻿
+function LoadContacts() {
+    $('#update_panel').html('Loading Data...');
+
+    $.ajax({
+        url: '/home/GetContacts',
+        type: 'GET',
+        dataType: 'json',
+        success: function (d) {
+            if(d,length > 0){
+                var $data = $('<table></table>').addClass('table table-bordered');
+                var header = "<thead><tr><th>Contact Person</th><th>Contact No</th><th>Country</th><th>State</th></tr></thead>";
+                $data.append(header);
+
+                $.each(d, function (i, row) {
+                    var $row = $('<tr/>');
+                    $row.append($('<td />').html(row.ContactPerson));
+                    $row.append($('<td />').html(row.ContactNo));
+                    $row.append($('<td />').html(row.CountryName));
+                    $row.append($('<td />').html(row.StateName));
+                    $row.append($('<td />').html("<a href='/home/save/'"+row.ContactID+"' class='btn btn'"));
+                });
+            }
+        },
+        error: function () {
+
+        }
+    });
+}
