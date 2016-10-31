d00e86e6a9688167cdbe019c2884ffbd4cbef96f
BANGKOKAIR\kritawit <kritawitbunket@gmail.com>
2016-10-31 16:56:39 +0700

    Add Update and Edit Method

---
+++ MVCPopupCRUD/Controllers/HomeController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPopupCRUD.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Save(int id = 0)
+        {
+            List<Country> Country = GetCountry();
+            List<State> States = new List<State>();
+
+            if (id > 0)
+            {
+                var c = GetContact(id);
+                if (c != null)
+                {
+                    ViewBag.Countries = new SelectList(Country, "CountryID", "CountryName", c.CountryID);
+                    ViewBag.States = new SelectList(GetState(c.CountryID), "StateID", "StateName", c.StateID);
+                }
+                else
+                {
+                    return HttpNotFound();
+                }
+                return PartialView("Save", c);
+
+            }
+            else
+            {
+                ViewBag.Countries = new SelectList(Country, "CountryID", "CountryName");
+                ViewBag.States = new SelectList(States, "StateID", "StateName");
+                return PartialView("Save");
+            }
+
+        }
+
+
+        public List<Country> GetCountry()
+        {
+            using (DBASPEntities dc = new DBASPEntities())
+            {
+                return dc.Countries.OrderBy(a => a.CountryName).ToList();
+            }
+        }
+
+        public List<State> GetState(int countryID)
+        {
+            using (DBASPEntities dc = new DBASPEntities())
+            {
+                return dc.States.Where(a => a.CountryID.Equals(countryID)).OrderBy(a => a.StateName).ToList();
+            }
+        }
+
+
+        public Contact GetContact(int contactID)
+        {
+            Contact contact = null;
+            using (DBASPEntities dc = new DBASPEntities())
+            {
+                var v = (from a in dc.Contacts
+                         join b in dc.Countries on a.CountryID equals b.CountryID
+                         join c in dc.States on a.StateID equals c.StateID
+                         select new
+                         {
+                             a,
+                             b.CountryName,
+                             c.StateName
+                         }).FirstOrDefault();
+
+                if (v != null)
+                {
+                    contact = v.a;
+                    contact.CountryName = v.CountryName;
+                    contact.StateName = v.StateName;
+                }
+
+                return contact;
+
+            }
+        }
+
+
+        public JsonResult GetContacts()
+        {
+            List<Contact> all = null;
+
+            using (DBASPEntities dc = new DBASPEntities())
+            {
+                var contacts = (from a in dc.Contacts
+                                join b in dc.Countries on a.CountryID equals b.CountryID
+                                join c in dc.States on a.StateID equals c.StateID
+                                select new
+                                {
+                                    a,
+                                    b.CountryName,
+                                    c.StateName
+                                });
+                if (contacts != null)
+                {
+                    all = new List<Contact>();
+                    foreach (var i in contacts)
+                    {
+                        Contact con = i.a;
+                        con.CountryName = i.CountryName;
+                        con.StateName = i.StateName;
+                        all.Add(con);
+                    }
+                }
+            }
+
+            return new JsonResult { Data = all, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+    }
+}
--- MVCPopupCRUD/Scripts/dotnetawesome.CRUDContacts.js
+++ MVCPopupCRUD/Scripts/dotnetawesome.CRUDContacts.js
@@ -1,13 +1,16 @@
-﻿
+﻿$(document).ready(function () {
+    LoadContacts();
+});
+
 function LoadContacts() {
-    $('#update_panel').html('Loading Data...');
+    $('#update_panel').html('<img src="/Content/loader.gif" />&nbsp; Loading Data...');
 
     $.ajax({
         url: '/home/GetContacts',
         type: 'GET',
         dataType: 'json',
         success: function (d) {
-            if(d,length > 0){
+            if (d, length > 0) {
                 var $data = $('<table></table>').addClass('table table-bordered');
                 var header = "<thead><tr><th>Contact Person</th><th>Contact No</th><th>Country</th><th>State</th></tr></thead>";
                 $data.append(header);
@@ -18,12 +21,18 @@ function LoadContacts() {
                     $row.append($('<td />').html(row.ContactNo));
                     $row.append($('<td />').html(row.CountryName));
                     $row.append($('<td />').html(row.StateName));
-                    $row.append($('<td />').html("<a href='/home/save/'"+row.ContactID+"' class='btn btn'"));
+                    $row.append($('<td />').html("<a href='/home/save/" + row.ContactID + "' class='btn btn-warning' data-toggle='modal' data-target='.popup' >Edit</a>&nbsp;|&nbsp;<a href='/home/Delete/" + row.ContactID + "' class='btn btn-danger' data-toggle='modal' data-target='.popup'>Delete</a>"));
+                    $data.append($row);
                 });
+
+                $('#update_panel').html($data);
+            } else {
+                var $noData = $('<div/>').html('No Data Found!');
+                $('#update_panel').html($noData);
             }
         },
         error: function () {
-
+            alert('Error! Please try again.');
         }
     });
 }
